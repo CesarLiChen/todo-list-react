ccee851de632decd4ed2c929cd5f7a0736f0b115
Cesar Li Chen <cesarli619@gmail.com>
2023-06-15 15:33:17 -0400

    Quick formatting fix

--- src/App.js
+++ src/App.js
@@ -59,7 +59,8 @@ function App(props) {
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
+        aria-labelledby="list-heading"
+      >
           {taskList}
       </ul>
     </div>
